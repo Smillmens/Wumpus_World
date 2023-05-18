f9016a24ac782334635e0a9becc7a1be661a7545
Sávio Milhomens <133260590+Smillmens@users.noreply.github.com>
2023-05-18 17:23:41 -0300

    Update WW_ETAPA1_FIXO.py

--- Etapas/Etapa 1/WW_ETAPA1_FIXO.py
+++ Etapas/Etapa 1/WW_ETAPA1_FIXO.py
@@ -1,27 +1,30 @@
-
 '''
 WUMPUS WORLD - ETAPA 1
-Objetivo: Criar um ambiente da Caverna com tamanho "N". A caverna deve conter os elementos
+
+Objetivo: Criar o ambiente da caverna.
+
+Com intuito de inicialização e ambientação ao projeto do Mundo de Wumpus, esse código
+gera o ambiente em forma de matriz de ordem 4 com os elementos distribuidos de forma 
+pré-definida:
+    - 1 Ouro;
+    - 3 Poços;
+    - 1 Wumpus.
+ 
 '''
 
-espaço = 6
-caverna = [[0 for _ in range(espaço)] for _ in range(espaço)]
+# Criando o ambiente Caverna:
+tm_caverna = 4 # Define a ordem da matriz quadrada <Caverna>
+caverna = [[0 for _ in range(tm_caverna)] for _ in range(tm_caverna)]
 
-# Definindo a posição do Wumpus
-caverna[0][1] = "(W)"
+# Distribuição e organização dos elementos
+caverna[3][3] = "(O)"       # Ouro posicionado na linha 0, coluna 3
+caverna[0][3] = "(P)"       # Poço posicionado na linha 0, coluna 3
+caverna[2][0] = "(P)"       # ||
+caverna[2][2] = "(P)"       # ||
+caverna[3][1] = "(W)"       # Wumpus posicionado na linha 0, coluna 1
 
-# Definindo a posição dos Poços
-caverna[1][0] = "(P)"
-caverna[1][2] = "(P)"
-caverna[3][3] = "(P)"
 
-# Definindo a posição do Ouro
-caverna[0][3] = "(O)"
-
-# Definindo a posição do Agente
-caverna[3][0] = "(A)"
-
-# Imprimindo o ambiente
+# Printado a caverna:
 for linha in caverna:
     for celula in linha:
         print(celula, end="\t")
